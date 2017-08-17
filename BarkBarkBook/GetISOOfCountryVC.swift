e7c2b15dd8bc124960d202e929a873668864f7d6
Dmytro <dipe@meta.com>
2017-08-17 16:13:39 +0300

    - add searchBar in GetISOOfCountryViewController

--- BarkBarkBook/GetISOOfCountryVC.swift
+++ BarkBarkBook/GetISOOfCountryVC.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class GetISOOfCountryViewController: UIViewController {
 
+    @IBOutlet weak var searchCountryTextField: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     var getCountryISO = [GetCountryISO]()
+    var filtered = [GetCountryISO]()
+    var searchActive = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +25,29 @@ class GetISOOfCountryViewController: UIViewController {
     @IBAction func ba_accept(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filtered = getCountryISO.filter({ (text) -> Bool in
+            let tmp: NSString = text.countryName as NSString
+            let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+        })
+        if filtered.count == 0 {
+            searchActive = false
+        } else {
+            searchActive = true
+        }
+        tableView.reloadData()
+    }
 }
 
 extension GetISOOfCountryViewController: UITableViewDataSource, UITextViewDelegate {
     //MARK: - TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(getCountryISO.count)
+        if searchActive {
+            return filtered.count
+        }
+        
         return getCountryISO.count
     }
     
@@ -36,8 +57,19 @@ extension GetISOOfCountryViewController: UITableViewDataSource, UITextViewDelega
         let countryName = getCountryISO[indexPath.row].countryName  // speciesDict.description
         let countryISOCode = getCountryISO[indexPath.row].countryISOCode
         
-        cell.countryNameLabel.text = String(describing: countryName)
-        cell.isoOfCountryIsHidden.text = String(countryISOCode)
+        if searchActive {
+                    }
+        
+        if searchActive {
+            let countryName2 = filtered[indexPath.row].countryName  // speciesDict.description
+            let countryISOCode2 = filtered[indexPath.row].countryISOCode
+
+            cell.countryNameLabel.text = String(describing: countryName2)
+            cell.isoOfCountryIsHidden.text = String(countryISOCode2)
+        } else {
+            cell.countryNameLabel.text = String(describing: countryName)
+            cell.isoOfCountryIsHidden.text = String(countryISOCode)
+        }
         
         return cell
     }
